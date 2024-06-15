db35b0e55503b77c2ececf4509dc29bf3017f189
Aaron Jones <aaron@jonesinator.com>
2024-06-15 10:41:15 -0600

    Typescript, Next.js, Tailwind UI minimal scaffold.

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
